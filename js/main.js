c454fd8230782e42632194c1d3d118711de06dea
Wesley Araújo Vieira <wesley.contato@hotmail.com>
2020-06-25 12:08:00 -0300

    Utilizando jQuery e Ajax - Parte 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-alert('Funcionou!')
+function consultaCep(){
+    var cep = document.getElementById("cep").value;
+    var url = "https://viacep.com.br/ws/" + cep + "/json/";
+    console.log(url);
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(response){
+            console.log(response);
+            //Exibindo os valores por id via Ajax (qtde. menor de linhas de código)
+            $("#logradouro").html(response.logradouro);
+            $("#bairro").html(response.bairro);
+            $("#localidade").html(response.localidade);
+            $("#uf").html(response.uf);
+
+            //document.getElementById("logradouro").innerHTML = response.logradouro;
+            //document.getElementById("bairro").innerHTML = response.bairro;
+            //document.getElementById("localidade").innerHTML = response.localidade;
+            //document.getElementById("uf").innerHTML = response.uf;
+
+        }
+    })
+}
